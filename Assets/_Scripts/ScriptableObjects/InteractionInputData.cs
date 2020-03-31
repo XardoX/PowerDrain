d3a49732e96ca58dbe990b4e6fcfc0af05d5b326
XardoX <jurek.gab.jg@gmail.com>
2020-04-01 01:34:57 +0200

    Started scripting interaction system

---
+++ Assets/_Scripts/ScriptableObjects/InteractionInputData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InteractionInputData", menuName = "Interaction System/Input Data")]
+public class InteractionInputData : ScriptableObject
+{
+    private bool _interactedClicked;
+    private bool _interactedRelease;
+
+    public bool InteractedClicked
+    {
+        get => _interactedClicked;
+        set => _interactedClicked = value;
+    }
+    public bool InteractedReleased
+    {
+        get => _interactedRelease;
+        set => _interactedRelease = value;
+    }
+
+    public void ResetInput()
+    {
+        _interactedClicked = false;
+        _interactedRelease = false;
+    }
+}
