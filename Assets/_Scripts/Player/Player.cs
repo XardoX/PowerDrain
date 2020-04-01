161d0e5004d04f825e3b4512ba0caabb66b6a136
XardoX <jurek.gab.jg@gmail.com>
2020-04-01 18:28:56 +0200

    Player.cs

---
+++ Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   public PlayerData data;
+   private float _energy;
+   private float _maxEnergy;
+
+   private bool _run;
+
+   private void Awake() 
+   {
+      _maxEnergy = data.maxEnergy;   
+   }
+   private void Start() 
+   {
+      GameController.instance.NextRun(_energy);
+   }
+
+   private void Update() 
+   {
+      
+   }
+}
