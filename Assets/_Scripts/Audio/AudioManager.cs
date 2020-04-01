e59afd5ea85bf0aac17969c4d74ab3ecd6799d4a
Som3one <47414520+Som3one@users.noreply.github.com>
2020-04-01 14:15:45 +0200

    Design changes
    Working on proper player controls
    Added two scripts for audio managing
    Turned off some options in Slim UI menu

---
+++ Assets/_Scripts/Audio/AudioManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sounds[] Sounds;
+
+    public static AudioManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        /*if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }*/
+
+        //DontDestroyOnLoad(gameObject);
+
+        foreach (Sounds s in Sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        
+    }
+
+  
+
+    public void Play(string name)
+    {
+        Sounds s = Array.Find(Sounds, Sound => Sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound" + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+    public void Stop(string name)
+    {
+        Sounds s = Array.Find(Sounds, Sound => Sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound" + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+}
