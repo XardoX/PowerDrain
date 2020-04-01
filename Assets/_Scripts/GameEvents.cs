07dd80fb4b04018a0b72463ff0906998c452cec3
XardoX <jurek.gab.jg@gmail.com>
2020-04-01 16:52:16 +0200

    gameevents

---
+++ Assets/_Scripts/GameEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+   public static GameEvents instance;
+
+   private void Awake() 
+   {
+       instance = this;
+   }
+
+   public event Action onDoorwayTriggerEnter;
+   public void OnDoorwayTriggerEnter()
+   {
+       if(onDoorwayTriggerEnter !=null)
+       {
+
+       }
+   }
+}
