550e91af279d7837a81b72225eb073cd24bb39c7
XardoX <jurek.gab.jg@gmail.com>
2020-04-01 15:03:11 +0200

    Interaction system base scripts

---
+++ Assets/_Scripts/ScriptableObjects/InteractionData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Interaction Data", menuName = "Interaction System/Interaction Data")]
+public class InteractionData : ScriptableObject
+{
+   private Interactable _interactable;
+
+   public Interactable InteractableObject
+   {
+       get => _interactable;
+       set => _interactable = value;
+   }
+
+   public void Interact()
+   {
+       _interactable.OnInteract();
+       ResetData();
+   }
+
+    public bool IsSameInteractable(Interactable _newInteractable) => _interactable == _newInteractable;
+    public bool IsEmpty() => _interactable == null;
+    public void ResetData() => _interactable = null;
+}
