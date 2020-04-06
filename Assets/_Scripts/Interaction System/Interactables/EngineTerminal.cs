53be670fddb239cc17a26fb4c82866be0677db8b
XardoX <jurek.gab.jg@gmail.com>
2020-04-06 15:02:52 +0200

    egnine termina

---
+++ Assets/_Scripts/Interaction System/Interactables/EngineTerminal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineTerminal : Interactable
+{
+    public override void OnInteract()
+    {
+       ObjectiveController.instance.SetObjective(0);
+    }
+}
